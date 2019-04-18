984bbe052460c938b6db0cdfc39c73cb8cd23fe3
abc <abc@example.com>
2019-04-18 18:32:53 +0200

    Header final

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,33 @@ import React from 'react';
 
 function Header() {
     return (
-        <header> header </header>
+        <header>
+            <div className="top-menu">
+                <div className="logo">
+                    <h1>Brewfinder</h1>
+                </div>
+
+                <nav>
+                    <ul>
+                        <li><a href="#Search">Search</a></li>
+                        <li><a href="#Search">Beer</a></li>
+                        <li><a href="#Search">Techs</a></li>
+                        <li><a href="#Search">About</a></li>
+
+                    </ul>
+                </nav>
+
+            </div>
+
+            <div className="main-banner">
+                    
+                    <h2> Your </h2>
+                    <h2> Favourite </h2>
+                    <h2> Breweries </h2>
+                    <p>  In one simple app</p>
+                    
+            </div>
+        </header>
     )
 }
 export default Header
