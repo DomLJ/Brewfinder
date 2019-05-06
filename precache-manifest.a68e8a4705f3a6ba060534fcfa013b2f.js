844ce379b90d8a708785cec5122f0a4ce186b6ca
DomLJ <dominik34@o2.pl>
2019-05-06 12:53:38 +0200

    Updates

---
+++ precache-manifest.a68e8a4705f3a6ba060534fcfa013b2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c477449f6115f998b7729ea19aebc0c",
+    "url": "/Brewfinder/index.html"
+  },
+  {
+    "revision": "e631ad24718feb440101",
+    "url": "/Brewfinder/static/css/main.62a15a7a.chunk.css"
+  },
+  {
+    "revision": "8422a76018da03b5b38e",
+    "url": "/Brewfinder/static/js/2.97d3f7fc.chunk.js"
+  },
+  {
+    "revision": "e631ad24718feb440101",
+    "url": "/Brewfinder/static/js/main.6ddf75e1.chunk.js"
+  },
+  {
+    "revision": "25ffb4ae064e7f451e6a",
+    "url": "/Brewfinder/static/js/runtime~main.61fe2796.js"
+  },
+  {
+    "revision": "508245a501df17a576165b6b02efb553",
+    "url": "/Brewfinder/static/media/claude-piche-1169584-unsplash.508245a5.jpg"
+  }
+]);
